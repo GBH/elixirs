12bcec593f6f706ccae22688590cd66a85a77fcb
Oleg <oleg@khabarov.ca>
2017-02-16 21:45:41 -0800

    some env config

--- sequence/lib/sequence.ex
+++ sequence/lib/sequence.ex
@@ -3,6 +3,6 @@ defmodule Sequence do
 
   def start(_type, _args) do
     # Starting main supervisor and sending initial value
-    {:ok, _pid} = Sequence.Supervisor.start_link(123)
+    {:ok, _pid} = Sequence.Supervisor.start_link(Application.get_env(:sequence, :initial_number))
   end
 end
